4a8b5c4f4c7b2e4eaf4b45d8ad2cdf2cbfbf1ece
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-08-01 16:14:31 +0000

    Done.

--- lib.rb/movie_cli.rb
+++ lib.rb/movie_cli.rb
@@ -1,11 +1,20 @@
+require_relative "../lib/scraper.rb"
+require.relative "../lib/theatre.rb"
 require 'pry'
+require 'nokogiri'
+
 class TheatreListingController
+  BASE_PATH = https://www.fandango.com/+ zip_movietimes?mode=general&q= + zip
+  ZIP = ^\d{5}$
 
 def call
   input = ""
   while input != "exit"
     puts "Welcome to your theatre directory!"
-    puts "Please enter your zip code:"
+    puts "Please enter your 5 digit zip code:"
     input = gets.strip
+      if input.length = 5
+      else
+        puts "Please try again:"
+
   end
-  
--- scraper.rb
+++ scraper.rb
@@ -4,7 +4,15 @@ require 'nokogiri'
 
 class Scraper
 
-  def self.scrape_movie_page(index_url)
+  def self.scrape_theatre_page(index_url)
     scraped_theatres = []
     html = open(index_url)
     doc = Nokogiri::HTML(html)
+
+  end
+
+  def self.scrape_movie_detail_page(profile_url)
+    scraped_theatres = {}
+
+  end
+end
